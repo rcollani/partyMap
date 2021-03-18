fdfd910ec9d6a5d38712fcaea1ad7d96f9b55e02
Robin von Collani <rvoncollani@techfak.uni-bielefeld.de>
2021-03-18 21:40:17 +0100

    More Chnages

--- MyTiles.tsx
+++ MyTiles.tsx
@@ -5,42 +5,42 @@
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
-  <image width="32" height="32" source="../../../../Pictures/Tiles_pngs/WoodFence_1.png"/>
+  <image width="32" height="32" source="Tiles_pngs/WoodFence_1.png"/>
  </tile>
  <tile id="1">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
-  <image width="32" height="32" source="../../../../Pictures/Tiles_pngs/WoodFence2-1.png.png"/>
+  <image width="32" height="32" source="Tiles_pngs/WoodFence2-1.png.png"/>
  </tile>
  <tile id="2">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
-  <image width="32" height="32" source="../../../../Pictures/Tiles_pngs/WoodFence3-1.png.png"/>
+  <image width="32" height="32" source="Tiles_pngs/WoodFence3-1.png.png"/>
  </tile>
  <tile id="4">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
-  <image width="32" height="32" source="../../../../Pictures/Tiles_pngs/Bush1-1.png.png"/>
+  <image width="32" height="32" source="Tiles_pngs/Bush1-1.png.png"/>
  </tile>
  <tile id="5">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
-  <image width="32" height="32" source="../../../../Pictures/Tiles_pngs/Brick-1.png.png"/>
+  <image width="32" height="32" source="Tiles_pngs/Brick-1.png.png"/>
  </tile>
  <tile id="6">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
-  <image width="32" height="32" source="../../../../Pictures/Tiles_pngs/WoodenBlock-1.png.png"/>
+  <image width="32" height="32" source="Tiles_pngs/WoodenBlock-1.png.png"/>
  </tile>
  <tile id="8">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
-  <image width="32" height="32" source="../../../../Pictures/Tiles_pngs/WoodenBlockDoor-1.png.png"/>
+  <image width="32" height="32" source="Tiles_pngs/WoodenBlockDoor-1.png.png"/>
  </tile>
 </tileset>
